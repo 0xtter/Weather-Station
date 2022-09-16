624becab43a25de1a12f912347aa955ed070523a
0xtter <41399587+0xtter@users.noreply.github.com>
2022-09-16 11:57:20 +0200

    Added Humidity Sensor display

--- Code/Weather-Station/Weather-Station.ino
+++ Code/Weather-Station/Weather-Station.ino
@@ -1,19 +1,22 @@
-#include "SevSeg.h"
+#include "SevSeg.h" //https://wokwi.com/projects/342943329015562835
 #include "DHT.h"
 
 #define DHTPIN 14
 #define DHTTYPE DHT11
 
 
-//Create an instance of the object.
 SevSeg sevseg;
 DHT dht(DHTPIN, DHTTYPE);
-
+  
 #define PATTERN_CHANGE_TIME 1000
 unsigned long timer = millis() - PATTERN_CHANGE_TIME;
 
+byte sensorSelector = 1;
+byte numberofSensors = 2;
+
 void setup() {
   Serial.begin(9600);
+  
   byte numDigits = 4;   
   byte digitPins[] = {13, 10, 9, 7}; //Digits: 1,2,3,4 <--put one resistor (ex: 220 Ohms, or 330 Ohms, etc, on each digit pin)
   byte segmentPins[] = {12, 8, 5, 3, 2, 11, 6, 4}; //Segments: A,B,C,D,E,F,G,Period
@@ -21,27 +24,31 @@ void setup() {
   byte hardwareConfig = COMMON_CATHODE; // See README.md for options
   bool updateWithDelays = false; // Default. Recommended
   bool leadingZeros = false; // Use 'true' if you'd like to keep the leading zeros
+  
 
+  // Initialize 4 digits 7 segments display
+  Serial.println("Initializing display...");
   sevseg.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments, updateWithDelays, leadingZeros);
   sevseg.setBrightness(90);
-  
-  // Initialise la capteur DHT11
+
+  // Initialize DHT11 sensors
+  Serial.println("Initializing sensors...");
   dht.begin();
 }
 
 void loop() {
-
   
-  // sur le moniteur serie
-  //Serial.println("Temperature = " + String(dht.readTemperature())+" °C");
-  //Serial.println("Humidite = " + String(dht.readHumidity())+" %");
-  char temp[5] = "15*C";//String(round(dht.readTemperature()))+"*C";
   if (millis() > (timer + PATTERN_CHANGE_TIME)) {
-    sevseg.setNumber(dht.readTemperature());
-    Serial.println(dht.readTemperature());
-    //Serial.println("Temperature = " + String(int(round(dht.readTemperature()))));
+    switch(sensorSelector){
+      case 1:
+        sevseg.setChars((String(round(dht.readTemperature()))+"*C").c_str());
+        break;
+      case 2:
+        sevseg.setChars((" "+String(round(dht.readHumidity()*10)/10)+"h").c_str());
+        break;
+    }
+    sensorSelector = ((sensorSelector++)%numberofSensors)+1;
     timer = millis();
   }
   sevseg.refreshDisplay();
-  //sevseg.refreshDisplay();
 }
